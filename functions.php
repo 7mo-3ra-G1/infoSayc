f43e647a8405135bf9aa6a3b7c2d78853a1ea9e2
lautmg <137836848+lautmg@users.noreply.github.com>
2023-10-02 21:16:57 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,17 +1,49 @@
 <?php
 
-include("conex.php");
+include("conexion.php");
 $linkconn = conectar();
 
-function getHorarios(){
+function getAula(){
     $linkconn = conectar();
-    $sql = "SELECT * FROM cronograma";
+    $sql = "SELECT aula.nombre, categoria.nombre AS categoria, aula.capacidad, aula.estado FROM aula INNER JOIN categoria ON aula.id_categoria = categoria.id_categoria;
+    ";
     return mysqli_query($linkconn, $sql);
 }
 
-function getProfesores(){
+function getCurso(){ 
     $linkconn = conectar();
-    $sql = "SELECT * FROM profesores";
+    $sql = "SELECT  curso.curso , curso.division  FROM curso ";
+    return  mysqli_query($linkconn, $sql);
+
+}
+
+function getCronograma(){
+    $linkconn = conectar();
+    $sql ="SELECT  cronogram.horario FROM cronogram";
+    return  mysqli_query($linkconn, $sql);
+
+}
+function getCategoria(){
+    $linkconn = conectar();
+    $sql = "SELECT  categoria.nombre FROM categoria";
+    return  mysqli_query($linkconn, $sql);
+
+}
+function getguias(){
+    $linkconn = conectar();
+    $sql = "SELECT  guias.nombre,guias.apellido,guias.curso FROM guias";
+    return  mysqli_query($linkconn, $sql);
+
+}
+function getTalleristas(){
+    $linkconn = conectar();
+    $sql = "SELECT  talleristas.nombre,talleristas.apellido FROM talleristas";
+    return  mysqli_query($linkconn, $sql);
+}
+
+function getGuiaCurso(){
+    $linkconn = conectar();
+    $sql = "SELECT curso.`curso`, curso.`division`, guias.nombre, guias.apellido FROM `curso` INNER JOIN guias ON curso.id_curso = guias.id_curso WHERE curso.id_curso = 3;";
     return  mysqli_query($linkconn, $sql);
 
 }
